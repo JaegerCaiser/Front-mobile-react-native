737d8627f1c655f7529c9288bd01272f04e560c7
Matheus Caiser <matheus.barrozo@qualirede.com.br>
2021-07-02 01:37:12 -0300

    Iniciado

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import Container, { Title, ForgotPassword, ForgotPasswordText } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('Funciona!!!');
+        }}
+      >
+        Entrar
+      </Button>
+
+      <ForgotPassword
+        onPress={() => {
+          console.log('ButtonPress');
+        }}
+      >
+        <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+      </ForgotPassword>
+    </Container>
+  );
+};
+
+export default SignIn;
